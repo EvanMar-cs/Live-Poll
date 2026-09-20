4f39fe7ec167901b252e0003ea7c144813b8e5f8
Jason Ballinger <jasonballinger_dev@yahoo.com>
2026-09-20 11:13:07 -0400

    update port to use env

--- app.py
+++ app.py
@@ -553,4 +553,4 @@ def handle_submit_vote(data):
 
 if __name__ == "__main__":
     load_applicants_and_profiles()
-    socketio.run(app, host="0.0.0.0", port=5001, debug=True)
+    socketio.run(app, host="0.0.0.0", port=os.getenv("PORT"), debug=True)
